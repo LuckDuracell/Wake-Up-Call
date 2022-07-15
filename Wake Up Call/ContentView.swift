d7a7148745edf66455acf46b023768ee504cbee6
LuckDuracell <lukedru@yahoo.com>
2022-07-15 11:16:29 -0700

    Initial Commit

---
+++ Wake Up Call/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Wake Up Call
+//
+//  Created by Luke Drushell on 7/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Wake Up Call/Wake_Up_CallApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wake_Up_CallApp.swift
+//  Wake Up Call
+//
+//  Created by Luke Drushell on 7/15/22.
+//
+
+import SwiftUI
+
+@main
+struct Wake_Up_CallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
